282a62158f41447b731192706c3c95f95f7aba02
ADM <adm@MacBook-Pro-de-ADM-2.local>
2020-06-22 22:00:04 -0300

    refact styles global for file globalStyles

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,8 @@
 import { AppProps } from "next/app";
 import { ApolloProvider } from "@apollo/react-hooks";
 import { useApollo } from "@utils/apollo";
-import { ThemeProvider, createGlobalStyle } from "styled-components";
+import { ThemeProvider } from "styled-components";
+import GlobalStyles from "@presentation/layout/GlobalStyles";
 
 const darkTheme = {
   background: "rgb(33, 44, 79)",
@@ -12,25 +13,6 @@ const lightTheme = {
   background: "#fff",
   text: "rgb(33, 44, 79)",
 };
-
-const GlobalStyles = createGlobalStyle`
-  * {
-    text-decoration: none;
-  }
-  html {
-      box-sizing: border-box;
-      -webkit-font-smoothing: antialiased;
-      font-size: 16px;
-  }
-  body {
-    font-size: 16px;
-    font-family: 'Montserrat', -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
-    background: ${(props) => props.theme.background};
-    color: ${(props) => props.theme.text};
-    overscroll-behavior: none;
-    overflow-x: hidden;
-  }
-`;
 
 function MyApp({ Component, pageProps }: AppProps) {
   const theme = darkTheme;
